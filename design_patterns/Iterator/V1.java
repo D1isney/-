6f31ed7f341ebcea0c9fabbdf4870e4b59fcc59d
D1isney <2497050447@qq.com>
2022-05-10 00:27:00 +0800

    迭代器
    Commit

--- design_patterns/Iterator/V1.java
+++ design_patterns/Iterator/V1.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 /*迭代器
  * 用来做遍历的
+ * 数组实现
  */
 
 public class V1 {
@@ -20,13 +21,18 @@ public class V1 {
 }
 
 //相比与数组 这个容器不用考虑边界 可以动态扩展
+//底层用数组实现
 class ArrayList_{
 	Object[] objects = new Object[10];
 	//objects中下一个空的位置在哪 或者说，目前容器中有多少个元素
 	private int index = 0;
 	public void add(Object o) {
+		
+		//判断容器的大小
 		if(index == objects.length) {
-			Object[] newObjects = new Object[objects.length + 2];
+											//jdk是按百分之50来扩
+											//动态扩展
+			Object[] newObjects = new Object[objects.length * 2];
 			System.arraycopy(objects, 0, newObjects, 0, objects.length);
 			objects = newObjects;
 		}
@@ -38,5 +44,4 @@ class ArrayList_{
 	public int size() {
 		return index;
 	}
-	
 }
---
+++ design_patterns/Iterator/V2.java
@@ -0,0 +1,20 @@
+package Iterator;
+
+import org.w3c.dom.Node;
+
+/*迭代器
+ * 链表实现
+ */
+
+public class V2 {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class LinkedList_{
+	Node head = null;
+	Node tail = null;
+	
+	
+}
