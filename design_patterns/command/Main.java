a220986ad2f0b20514574cd8c1ed8e4fea4592d9
D1isney <2497050447@qq.com>
2022-05-16 10:55:34 +0800

    Command
    Commit

--- design_patterns/command/Main.java
+++ design_patterns/command/Main.java
@@ -1,5 +1,8 @@
 package command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*Action动作、Transaction事物
  * 
  *宏命令
@@ -41,5 +44,25 @@ public class Main {
 		delCommand.undo();
 		System.out.println(c.msg);
 		
+		System.out.println();
+		System.out.println("责任链实现连续undo");
+		//责任链实现连续的undo
+		List<Command> commands = new ArrayList<>();
+		commands.add(new InsertCommand(c));
+		commands.add(new CopyCommand(c));
+		commands.add(new DeleteCommand(c));
+		
+		for(Command co:commands) {
+			co.doit();
+		}
+		
+		System.out.println(c.msg);
+		
+		//undo一定得倒序来
+		for(int i = commands.size()-1;i>=0;i--) {
+			commands.get(i).undo();
+		}
+		
+		System.out.println(c.msg);
 	}
 }
